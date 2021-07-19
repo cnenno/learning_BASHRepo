e5bf1ecc0cf88f3e2d9f1e4f0d69d12de90f02fe
ThatCyberGuy5 <61467648+cnenno@users.noreply.github.com>
2021-07-19 06:56:10 -0700

    change
    change

--- netcat scan run1.sh
+++ netcat scan run1.sh
@@ -5,7 +5,7 @@ do
 	while [ $x -lt 81 ]
 	do nc -w1 -nvz 10.50.0.$y $x > /dev/null 2>&1
 		if [[ $? == 0 ]]; then
-			echo "$x is open on 10.50.0.$y" >> /home/rees/results.txt
+			echo "$x is open on 10.50.0.$y" >> /home/results.txt
 		fi
 	let x=(x+1)
 done
